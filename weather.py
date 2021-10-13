522aa6d4ff02598d2478fb74d3c338691b5a382e
Aleksandr Loskutov <aloskutyan@gmail.com>
2021-10-13 23:45:55 +0300

    added wind bearing texts and weather emoji + refactoring

--- weather.py
+++ weather.py
@@ -5,15 +5,13 @@ https://tech.yandex.com/weather/
 
 """
 
-
 import asyncio
 import logging
 import socket
+from datetime import timedelta
 
 import aiohttp
 import async_timeout
-
-from datetime import timedelta
 import voluptuous as vol
 import homeassistant.util.dt as dt_util
 
@@ -25,7 +23,6 @@ from homeassistant.components.weather import (
     ATTR_FORECAST_WIND_SPEED, ATTR_WEATHER_PRESSURE, ATTR_WEATHER_HUMIDITY, PLATFORM_SCHEMA, WeatherEntity)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.util import Throttle
-
 
 import homeassistant.helpers.config_validation as cv
 
@@ -42,6 +39,8 @@ ATTR_OBS_TIME = 'observation_time'
 ATTR_WEATHER_CON = 'weather_condition'
 ATTR_PRECIPITATION_PROB = 'precipitation_probability'
 ATTR_WIND_SPEED_MS = 'wind_speed_ms'
+ATTR_WIND_BEARING_TXT = 'wind_bearing_txt'
+ATTR_WEATHER_CON_EMOJI = 'weather_condition_emoji'
 
 CONDITION_CLASSES = {
     'sunny': ['clear'],
@@ -73,33 +72,69 @@ DESCRIPTION_DIC = {
     'wet-snow': 'Дождь со снегом',
     'snow': 'Снег',
     'snow-showers': 'Снегопад',
-    'light-snow': 'Небольшой снег', 
+    'light-snow': 'Небольшой снег',
+}
+
+WIND_BEARINGS = {
+    'nw': 'Северо-западный',
+    'n': 'Северный',
+    'ne': 'Северо-восточный',
+    'e': 'Восточный',
+    'se': 'Юго-восточный',
+    's': 'Южный',
+    'sw': 'Юго-западный',
+    'w': 'Западный',
+    'c': 'Практически отсутствует'
+}
+
+WEATHER_CONDITIONS_EMOJI = {
+    'clear': '\U00002600',
+    'partly-cloudy': '\U0001F324',
+    'cloudy': '\U000026C5',
+    'overcast': '\U00002601',
+    'heavy-rain': '\U0001F327\U00002614',
+    'continuous-heavy-rain': '\U0001F327\U0001F327',
+    'showers': '\U0001F327\U00002614',
+    'hail': '\U0001F327\U00002614',
+    'drizzle': '\U0001F327\U0001F302',
+    'light-rain': '\U0001F327\U0001F302',
+    'rain': '\U0001F327',
+    'moderate-rain': '\U0001F327',
+    'thunderstorm': '\U0001F329',
+    'thunderstorm-with-rain': '\U000026C8',
+    'thunderstorm-with-hail': '\U000026C8',
+    'wet-snow': '\U0001F327\U00002744',
+    'snow': '\U0001F328',
+    'snow-showers': '\U0001F328\U0001F328',
+    'light-snow': '\U0001F328'
 }
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=30)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({    
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Required(CONF_API_KEY): cv.string,
     vol.Optional(CONF_LATITUDE): cv.latitude,
     vol.Optional(CONF_LONGITUDE): cv.longitude,
 })
 
+
 async def async_setup_platform(
     hass, config, async_add_entities, discovery_info=None):
-
     """Set up the Yandex.Weather weather platform."""
     longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
     latitude = config.get(CONF_LATITUDE, hass.config.latitude)
     name = config.get(CONF_NAME)
     api_key = config.get(CONF_API_KEY)
     session = async_get_clientsession(hass)
-    loop = hass.loop    
+    loop = hass.loop
 
     async_add_entities([YandexWeather(name, longitude, latitude, api_key, loop, session)], True)
 
-class YandexWeather (WeatherEntity):
+
+class YandexWeather(WeatherEntity):
     """Representation of a weather entity."""
+
     def __init__(self, name: str, longitude: str, latitude: str, api_key: str, loop, session):
         self._name = name
         self._longitude = longitude
@@ -107,13 +142,14 @@ class YandexWeather (WeatherEntity):
         self._api_key = api_key
         self._hloop = loop
         self._hsession = session
-        self._weather_data = YaWeather(self._latitude, self._longitude, self._api_key, self._hloop, session=self._hsession)
-    
+        self._weather_data = YaWeather(self._latitude, self._longitude, self._api_key, self._hloop,
+                                       session=self._hsession)
+
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     async def async_update(self):
         """Get the latest weather information."""
         await self._weather_data.get_weather()
-    
+
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
@@ -125,19 +161,19 @@ class YandexWeather (WeatherEntity):
         if self._weather_data.current is not None:
             return self._weather_data.current.get('temp')
         return None
-    
+
     @property
     def temperature_unit(self) -> str:
         """Return the unit of measurement."""
         return TEMP_CELSIUS
-    
+
     @property
     def humidity(self) -> int:
         """Return the humidity."""
         if self._weather_data.current is not None:
             return self._weather_data.current.get('humidity')
         return None
-    
+
     @property
     def wind_speed(self) -> float:
         """Return the wind speed."""
@@ -145,28 +181,28 @@ class YandexWeather (WeatherEntity):
             # Convert from m/s to km/h
             return round(self._weather_data.current.get('wind_speed') * 18 / 5)
         return None
-    
+
     @property
     def wind_bearing(self) -> str:
-        """Return the wind speed."""
+        """Return the wind bearing."""
         if self._weather_data.current is not None:
             # The current wind bearing
             return self._weather_data.current.get('wind_dir')
         return None
-    
+
     @property
     def pressure(self) -> int:
         """Return the pressure."""
         if self._weather_data.current is not None:
             return self._weather_data.current.get('pressure_pa')
-        return None    
-    
+        return None
+
     @property
     def condition(self) -> str:
         if self._weather_data.current is not None:
             return next((
-            k for k, v in CONDITION_CLASSES.items()
-            if self._weather_data.current.get('condition') in v), None)
+                k for k, v in CONDITION_CLASSES.items()
+                if self._weather_data.current.get('condition') in v), None)
         return STATE_UNKNOWN
 
     @property
@@ -174,7 +210,7 @@ class YandexWeather (WeatherEntity):
         """Return the pressure."""
         if self._weather_data.current is not None:
             return self._weather_data.current.get('icon')
-        return None 
+        return None
 
     @property
     def forecast(self):
@@ -184,10 +220,10 @@ class YandexWeather (WeatherEntity):
             fc_array = self._weather_data.forecast.get('parts', [])
             for data_in in fc_array:
                 data_out = {}
-                if (list(fc_array).index(data_in) == 0):
-                    data_out[ATTR_FORECAST_TIME] = dt_util.utcnow()+timedelta(minutes=350)
-                if (list(fc_array).index(data_in) == 1):
-                    data_out[ATTR_FORECAST_TIME] = dt_util.utcnow()+timedelta(minutes=700)
+                if list(fc_array).index(data_in) == 0:
+                    data_out[ATTR_FORECAST_TIME] = dt_util.utcnow() + timedelta(minutes=350)
+                if list(fc_array).index(data_in) == 1:
+                    data_out[ATTR_FORECAST_TIME] = dt_util.utcnow() + timedelta(minutes=700)
                 data_out[ATTR_FORECAST_TEMP] = data_in.get('temp_max')
                 data_out[ATTR_FORECAST_TEMP_LOW] = data_in.get('temp_min')
                 data_out[ATTR_FORECAST_CONDITION] = next((
@@ -196,7 +232,8 @@ class YandexWeather (WeatherEntity):
                 data_out[ATTR_PRESSURE_MM] = data_in.get('pressure_mm')
                 data_out[ATTR_WEATHER_ICON] = data_in.get('icon')
                 data_out[ATTR_FEELS_LIKE] = data_in.get('feels_like')
-                data_out[ATTR_FORECAST_WIND_SPEED] = round(data_in.get('wind_speed') * 18 / 5) if 'wind_speed' in data_in else None
+                data_out[ATTR_FORECAST_WIND_SPEED] = (round(data_in.get('wind_speed') * 18 / 5)
+                                                      if 'wind_speed' in data_in else None)
                 data_out[ATTR_FORECAST_WIND_BEARING] = data_in.get('wind_dir')
                 data_out[ATTR_FORECAST_PRECIPITATION] = data_in.get('prec_mm')
                 data_out[ATTR_PRECIPITATION_PROB] = data_in.get('prec_prob')
@@ -204,16 +241,18 @@ class YandexWeather (WeatherEntity):
                 data_out[ATTR_WEATHER_PRESSURE] = data_in.get('pressure_pa')
                 data_out[ATTR_WEATHER_HUMIDITY] = data_in.get('humidity')
                 data_out[ATTR_WIND_SPEED_MS] = data_in.get('wind_speed')
+                data_out[ATTR_WEATHER_CON_EMOJI] = WEATHER_CONDITIONS_EMOJI[data_in.get('condition')]
+                data_out[ATTR_WIND_BEARING_TXT] = WIND_BEARINGS[data_in.get('wind_dir')]
                 data_out['part_of_day'] = data_in.get('part_name')
                 fcdata_out.append(data_out)
 
             return fcdata_out
-    
+
     @property
     def attribution(self) -> str:
         """Return the attribution."""
         return ATTRIBUTION
-    
+
     @property
     def device_state_attributes(self):
         """Return device specific state attributes."""
@@ -223,10 +262,13 @@ class YandexWeather (WeatherEntity):
             data[ATTR_PRESSURE_MM] = self._weather_data.current.get('pressure_mm')
             data[ATTR_WIND_SPEED_MS] = self._weather_data.current.get('wind_speed')
             data[ATTR_WEATHER_ICON] = self._weather_data.current.get('icon')
-            data[ATTR_OBS_TIME] = dt_util.as_local(dt_util.utc_from_timestamp(self._weather_data.current.get('obs_time'))).strftime(TIME_STR_FORMAT)
+            data[ATTR_OBS_TIME] = dt_util.as_local(
+                dt_util.utc_from_timestamp(self._weather_data.current.get('obs_time'))).strftime(TIME_STR_FORMAT)
             data[ATTR_WEATHER_CON] = DESCRIPTION_DIC[self._weather_data.current.get('condition')]
+            data[ATTR_WEATHER_CON_EMOJI] = WEATHER_CONDITIONS_EMOJI[self._weather_data.current.get('condition')]
+            data[ATTR_WIND_BEARING_TXT] = WIND_BEARINGS[self._weather_data.current.get('wind_dir')]
             return data
-        return None 
+        return None
 
 
 class YaWeather(object):
@@ -241,11 +283,10 @@ class YaWeather(object):
         self._session = session
         self._current = None
         self._forecast = None
-        
 
     async def get_weather(self):
-        base_url="https://api.weather.yandex.ru/v2/informers?lat=%s&lon=%s" % (self._lat, self._lon)
-        headers = {'X-Yandex-API-Key':self._api}       
+        base_url = "https://api.weather.yandex.ru/v2/informers?lat=%s&lon=%s" % (self._lat, self._lon)
+        headers = {'X-Yandex-API-Key': self._api}
 
         try:
             async with async_timeout.timeout(5, loop=self._loop):
@@ -258,7 +299,7 @@ class YaWeather(object):
                 _LOGGER.debug(f"Current data:{self._current}")
                 _LOGGER.debug(f"Forecast data:{self._forecast}")
             else:
-                _LOGGER.error('Error fetching data from Yandex.Weather, %s, %s', data['status'],data['message'])
+                _LOGGER.error('Error fetching data from Yandex.Weather, %s, %s', data['status'], data['message'])
 
         except (asyncio.TimeoutError,
                 aiohttp.ClientError, socket.gaierror) as error:
